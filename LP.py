14124b1e5b1cb8d26c6b94b016b812caa58096ff
like <like@likedeMacBook-Pro.local>
2017-10-12 00:40:03 +0800

    能够在终端输出需要的第一页文字

--- LP.py
+++ LP.py
@@ -1,1 +1,12 @@
-x#Little Reptile using to get information of exchange programs automatically
+#Little Reptile using to get information of exchange programs automatically
+import urllib.request
+from lxml import html
+import requests
+
+response = requests.get("http://bksy.zju.edu.cn/dwjlfwpt/redir.php?catalog_id=1064225")
+response.encoding = 'gbk'
+tree = html.fromstring(response.text)
+
+title = tree.xpath('//a[@class="highlight"]/text()')
+for i in title:
+	print(i)
